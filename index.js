7b5358dfe5927b6dec0d68dc21af6ab41cda93c7
Noah Holloway <njh314159@gmail.com>
2022-11-16 22:45:52 -0500

    basic fitts law

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express')
 var path = require('path')
 var app = express();
+var hbs = require('hbs');
 
 app.use('/js', express.static(path.join(__dirname, '/js')))
 app.use('/css', express.static(path.join(__dirname, '/css')))
 
+app.set('view engine', 'hbs');
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/test', function(req, res){
+    var id = req.query.id;
+    console.log(id);
+    var data = {
+        circles: 11,
+        inner_radius: 50,
+        outer_radius: 300,
+        mouse_speed: 2
+    }
+    res.render('fittstest', data);
 });
 
 var listener = app.listen(process.env.PORT || 8080, process.env.HOST || "0.0.0.0", function() {
--- js/fittstest.js
+++ js/fittstest.js
@@ -1,3 +1,48 @@
+/*
+    global variables
+    circles
+    inner_radius
+    outer_radius
+    mouse_speed
+*/
+
+/*
+    draws a set of circles around the screen, based on @global circles. If index_to_highlight is not -1,
+    highlights the Nth circle in the chain (n \in [0, circles). )
+*/
+
+var current_num = 0;
+var last_was_correct = 1;
+
+function draw_circles (index_to_highlight) {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    let midX = canvas.width / 2;
+    let midY = canvas.height / 2;
+    for (i = 0; i < circles; i++) {
+        angle = i * 2 * Math.PI / (circles);
+        cX = midX + outer_radius * Math.cos(angle);
+        cY = midY + outer_radius * Math.sin(angle);
+        if (i == index_to_highlight) {
+            context.beginPath();
+            context.arc(cX, cY, inner_radius, 0, 2 * Math.PI, false);
+            context.fillStyle = "#FFFFFF";
+            context.fill();
+        } else {
+            //context.fillStyle = "#FFBBBB";
+        }
+        
+        
+        context.strokeStyle = "#000000";
+        context.beginPath();
+        context.arc(cX, cY, inner_radius, 0, 2 * Math.PI, false);
+        context.lineWidth = 2;
+        context.stroke();
+    }
+
+}
+
 x = function() {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
@@ -16,14 +61,16 @@ x = function() {
     document.addEventListener('pointerlockchange', lockChangeAlert, false);
     document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
 
-    document.addEventListener("keydown", (e) => {
+    /*document.addEventListener("keydown", (e) => {
         if (e.code == "ArrowLeft") {
             sF = Math.max(0.2, sF - 0.2);
         } else if (e.code == "ArrowRight") {
             sF = Math.min(5, sF + 0.2)
         }
         drawStuff();
-    }, false);
+    }, false);*/
+
+    
 
     function lockChangeAlert() {
         if (document.pointerLockElement === canvas ||
@@ -36,10 +83,10 @@ x = function() {
         }
     }
 
-    let virtualx = canvas.width / 2;
-    let virtualy = canvas.height / 2;
+    let virtualx = canvas.width / 2.0;
+    let virtualy = canvas.height / 2.0;
 
-    let sF = 1;
+    let sF = mouse_speed;
 
     function updatePosition(e) {
         dX = e.movementX;
@@ -47,6 +94,7 @@ x = function() {
 
         virtualx += sF * dX;
         virtualy += sF * dY;
+        console.log(virtualx);
 
         if (virtualx < 0) {
             virtualx = canvas.width;
@@ -60,9 +108,29 @@ x = function() {
             virtualy = 0;
         }
 
-        drawStuff();
+        drawStuff(virtualx, virtualy);
     }
 
+    canvas.addEventListener("mousedown", (e) => {
+        let midX = canvas.width / 2;
+        let midY = canvas.height / 2;
+        
+        angle = current_num * 2 * Math.PI / (circles);
+        cX = midX + outer_radius * Math.cos(angle);
+        cY = midY + outer_radius * Math.sin(angle);
+
+        console.log(cX, cY, virtualx, virtualy);
+
+        dist_sq = Math.pow(cX - virtualx, 2) + Math.pow(cY - virtualy, 2)
+        if (dist_sq > inner_radius * inner_radius) {
+            last_was_correct = 0;
+        } else {
+            last_was_correct = 1;
+        }
+
+        current_num = (current_num + Math.floor(circles / 2) + 1) % circles;
+        drawStuff();
+    })
 
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
@@ -80,15 +148,27 @@ x = function() {
     
     resizeCanvas();
           
-    function drawStuff() {
-        context.fillStyle = "#BBBBFF";
+    function drawStuff(a, b) {
+        if (last_was_correct) {
+            context.fillStyle = "#BBFFBB";
+        } else {
+            context.fillStyle = "#FFBBBB";
+        }
         context.fillRect(0, 0, canvas.width, canvas.height);
+
+        draw_circles(current_num);
 
         context.font = "20px serif"
         context.fillStyle = "black";
         context.fillText("Press → to increase scale factor, ← to decrease, click to interact", 10, 30);
         context.fillText("Window size: " + canvas.width + " " + canvas.height, 10, 50);
         context.fillText("Current SF: " + (parseFloat(sF).toPrecision(2)), 10, 70);
+        if (last_was_correct) {
+            context.fillText("Previous click: Success", 10, 90);
+        } else {
+            context.fillText("Previous click: Fail", 10, 90);
+        }
+        
 
         context.beginPath();
         context.arc(virtualx, virtualy, 5, 0, 2 * Math.PI, false);
@@ -98,6 +178,8 @@ x = function() {
     }
 }
 
+
 $(document).ready(() => {
     x();
+    console.log();
 });
