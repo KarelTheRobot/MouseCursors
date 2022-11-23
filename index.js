f3c67565c0b668d3867f49126ab3880dba275725
Noah Holloway <njh314159@gmail.com>
2022-11-22 21:31:09 -0500

    push to prod

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var hbs = require('hbs');
 var bodyParser = require('body-parser');
 var cookie_session = require('cookie-session');
 var mysql = require('mysql');
+var fs = require('fs');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cookie_session({
-    httpOnly: false,
     name: 'session',
-    keys: ["alksdjfaasklejghuoihbvauodh", "amsndmabvkhoiuhuiopepoajkhnfjgahfbc"]
+    keys: ["alksdjfaasklejghuoihbvauodh", "amsndmabvkhoiuhuiopepoajkhnfjgahfbc"],
+    cookie: {secure: false}
   }))
 
 app.use('/js', express.static(path.join(__dirname, '/js')))
@@ -20,14 +22,15 @@ app.use('/css', express.static(path.join(__dirname, '/css')))
 app.set('view engine', 'hbs');
 
 app.set('trust proxy', 1) // trust first proxy 
-app.get("/cookie_test", function(req, res){
+/*app.get("/cookie_test", function(req, res){
     if (req.session.token == undefined) {
         req.session.token = 5;
     } else {
+        req.session.token += 1;
         console.log(req.session.token);
     }
     res.send("hello!");
-})
+})*/
 
 function generate_order(num) {
     function shuffleArray(array) {
@@ -41,23 +44,27 @@ function generate_order(num) {
     return arr;
 }
 
+app.get("/clear_all", function(req, res) {
+    req.session.completed_bits = 0;
+    req.session.id = undefined;
+    res.redirect("/");
+})
+
 app.get('/', function(req, res){
+
     var id = req.session.id;
 
     if (id == undefined) {
         res.redirect("/login");
     } else {
-
-        
         var order = req.session.order;
         if (order == undefined) {
             order = generate_order(5);
             req.session.order = order;
         }
-        var completed = req.session.completed;
-        if (completed == undefined) {
-            completed = [0, 0, 0, 0, 0];
-            req.session.completed = completed;
+
+        if (req.session.completed_bits == undefined) {
+            req.session.completed_bits = 0;
         }
         /*var completed0 = req.session.completed[0] || 0;
         var completed1 = req.session.completed[1] || 0;
@@ -65,15 +72,12 @@ app.get('/', function(req, res){
         var completed3 = req.session.completed[3] || 0;
         var completed4 = req.session.completed[4] || 0;*/
 
-        console.log(req.session.completed);
-        console.log(req.session.order);
-
         text1 = "<div id='main_div'>"
         cutoff = false;
         for (i = 1; i <= 5; i++) {
             text1 +=    "<a class=\"btn "
-            if (req.session.completed[i-1]) {
-                text1 += "btn-outline-success disabled\""
+            if (req.session.completed_bits & (2 << (i - 1))) {
+                text1 += "btn-secondary disabled\""
             } else if (!cutoff) {
                 cutoff = true;
                 text1 += "btn-primary\""
@@ -83,8 +87,12 @@ app.get('/', function(req, res){
             text1 += " href=\"/test?id=" + i + "\" role=\"button\">Part " + i + "</a>"
             //text1 +=    "<a href=\"/test?id=" + i + "\">Part " + i + "</a></button>"
         }
-        text1 += "</div>"
+        text1 += "<a class=\"btn btn-danger\" href=\"/clear_all\" role=\"button\">Start Over</a> </div>"
         
+        if (!cutoff) {
+            text1 += "<p>All done! Make sure you've taken the survey <a href=\"https://docs.google.com/forms/d/e/1FAIpQLSc68eLjBw_8BNFndxkZlVOCn3Gv3sEKdBO4TXgJD8wL2MI_wQ/viewform?usp=sf_link\">here</a>. Thank you!</p>"
+        }
+
         data = {
             id: id,
             inj: text1
@@ -99,7 +107,6 @@ app.get("/login", function(req, res){
 })
 
 app.post("/register", function(req, res){
-    console.log(req.body.compid);
     req.session.id = req.body.compid;
     res.redirect("/");
 })
@@ -109,29 +116,40 @@ app.get('/test', function(req, res){
     var index = req.query.id;
     var id = req.session.id;
     var order = req.session.order;
+    var completed_bits = req.session.completed_bits;
     if (order == undefined) {
-        order = generate_order(5);
-        req.session.order = order;
-    }
-    var completed = req.session.completed;
-    if (completed == undefined) {
-        completed = [0, 0, 0, 0, 0];
-        req.session.completed = completed;
+        res.redirect("/");
+    } else if (completed_bits == undefined) {
+        res.redirect("/");
+    } else {
+        var data = {
+            circles: 11,
+            inner_radius: 50,
+            outer_radius: 300,
+            mouse_speed: req.session.order[index-1],
+            index: index
+        }
+        res.render('fittstest', data);
     }
 
-    console.log(id);
-    var data = {
-        circles: 11,
-        inner_radius: 50,
-        outer_radius: 300,
-        mouse_speed: req.session.order[index-1]
-        //index: index
-    }
-    res.render('fittstest', data);
+    
 });
 
 app.post("/result", function(req, res){
-    console.log(req.body);
+    o = req.session.order;
+
+    string = req.body.index + "," + o[req.body.index - 1] + "," + req.session.id + "," + String(req.body.average_time) + "," + String(req.body.first_5_avg) + "," + String(req.body.last_5_avg) + "," + String(req.body.total_correct) + "," + String(req.body.percent_correct) + "\n";
+    fs.appendFileSync('output.csv', string);
+    console.log(string);
+    
+    a = req.session.completed_bits;
+
+    index = req.body.index - 1;
+
+    a |= (2 << index);
+
+    req.session.completed_bits = a;
+
     res.send("hi");
 })
 
--- js/fittstest.js
+++ js/fittstest.js
@@ -10,12 +10,19 @@
     draws a set of circles around the screen, based on @global circles. If index_to_highlight is not -1,
     highlights the Nth circle in the chain (n \in [0, circles). )
 */
+const REPETITION_FACTOR = 3;
 
 var current_num = 0;
+var circles_clicked = 0;
 var last_was_correct = 1;
 var last_click_time = -1;
 var start_time = -1;
 var total_correct = 0;
+var average_time = 0;
+var first_5_start = -1;
+var last_5_start = -1;
+var first_5_avg = -1;
+var last_5_avg = -1;
 
 var game_is_active = false;
 var game_is_over = false;
@@ -60,7 +67,7 @@ x = function() {
                            document.mozExitPointerLock;
 
     canvas.onclick = function() {
-        console.log("hi");
+        //console.log("hi");
         if (!game_is_over) {
             canvas.requestPointerLock()
         }
@@ -83,10 +90,10 @@ x = function() {
     function lockChangeAlert() {
         if (document.pointerLockElement === canvas ||
                 document.mozPointerLockElement === canvas) {
-            console.log('The pointer lock status is now locked');
+            //console.log('The pointer lock status is now locked');
             document.addEventListener("mousemove", updatePosition, false);
         } else {
-            console.log('The pointer lock status is now unlocked');  
+            //console.log('The pointer lock status is now unlocked');  
             document.removeEventListener("mousemove", updatePosition, false);
         }
     }
@@ -104,15 +111,19 @@ x = function() {
         virtualy += sF * dY;
 
         if (virtualx < 0) {
-            virtualx = canvas.width;
-        } else if (virtualx > canvas.width) {
             virtualx = 0;
+            //virtualx = canvas.width;
+        } else if (virtualx > canvas.width) {
+            //virtualx = 0;
+            virtualx = canvas.width;
         }
 
         if (virtualy < 0) {
-            virtualy = canvas.height;
-        } else if (virtualy > canvas.height) {
+            //irtualy = canvas.height;
             virtualy = 0;
+        } else if (virtualy > canvas.height) {
+            //virtualy = 0;
+            virtualy = canvas.height;
         }
 
         drawStuff(virtualx, virtualy);
@@ -127,7 +138,7 @@ x = function() {
             cX = midX + outer_radius * Math.cos(angle);
             cY = midY + outer_radius * Math.sin(angle);
 
-            console.log(cX, cY, virtualx, virtualy);
+            //console.log(cX, cY, virtualx, virtualy);
 
             dist_sq = Math.pow(cX - virtualx, 2) + Math.pow(cY - virtualy, 2)
             if (dist_sq > inner_radius * inner_radius) {
@@ -140,14 +151,55 @@ x = function() {
             //current_num = (current_num + Math.floor(circles / 2) + 1) % circles;
             
 
-            if (current_num == 0) {
+            if (circles_clicked == 0) {
                 start_time = Date.now();
+                first_5_start = start_time;
+
+                console.log("start time: ", start_time)
+            } else if (circles_clicked == (circles * REPETITION_FACTOR - 5)) {
+                last_5_start = Date.now();
+                console.log("last 5 start time: ", last_5_start)
             }
 
-            current_num += 1;
+            circles_clicked += 1;
+            current_num += Math.floor(circles / 2) + 1;
+            current_num %= circles;
 
-            if (current_num == circles) {
-                end_game();
+            if (circles_clicked == 5) {
+                first_5_end = Date.now();
+                df = first_5_end - first_5_start;
+                first_5_avg = df / (5 - 1); // -1 since there are only 4 windows to click
+                console.log("first 5 end time: ", first_5_end);
+                console.log("first 5 average: ", first_5_avg);
+            } else if (circles_clicked == circles * REPETITION_FACTOR) {
+                last_5_end = Date.now();
+                df = last_5_end - last_5_start;
+                last_5_avg = df / (5 - 1);
+
+                console.log("last 5 end: ", last_5_end);
+                console.log("last 5 average: ", last_5_avg);
+
+                end_time = Date.now();
+                time_elapsed = last_5_end - start_time;
+                average_time = time_elapsed / (REPETITION_FACTOR * circles - 1);
+                console.log("overall average: ", average_time);
+
+                res = {
+                    average_time: average_time,
+                    total_correct: total_correct,
+                    percent_correct: total_correct/(REPETITION_FACTOR * circles),
+                    first_5_avg: first_5_avg,
+                    last_5_avg: last_5_avg,
+                    index: index
+                }
+                console.log(res);
+
+                game_is_active = false;
+                game_is_over = true;
+                document.exitPointerLock();
+                console.log("ending game!");
+
+                post_result(res)
             }
 
             drawStuff();
@@ -171,28 +223,20 @@ x = function() {
     })
 
     function init_game() {
+        circles_clicked = 0;
         current_num = 0;
         last_was_correct = 1;
         last_click_time = -1;
         total_correct = 0;
         game_is_active = true;
+
+        first_5_start = -1;
+        last_5_start = -1;
+        first_5_avg = -1;
+        last_5_avg = -1;
     }
 
-    function end_game() {
-        end_time = Date.now();
-        time_elapsed = end_time - start_time;
-        average_time = time_elapsed / (circles - 1);
-
-        res = {
-            average_time: average_time,
-            total_correct: total_correct,
-        }
-
-        game_is_active = false;
-        game_is_over = true;
-        document.exitPointerLock();
-        console.log("ending game!");
-
+    function post_result(res) {
         $.post("/result", res, function(data) {
             console.log("success!", data);
         })
@@ -227,13 +271,13 @@ x = function() {
 
             context.font = "20px serif"
             context.fillStyle = "black";
-            context.fillText("Press → to increase scale factor, ← to decrease, click to interact", 10, 30);
-            context.fillText("Window size: " + canvas.width + " " + canvas.height, 10, 50);
-            context.fillText("Current SF: " + (parseFloat(sF).toPrecision(2)), 10, 70);
+            //context.fillText("Press → to increase scale factor, ← to decrease, click to interact", 10, 30);
+            context.fillText("Window size: " + canvas.width + " " + canvas.height, 10, 30);
+            //context.fillText("Current SF: " + (parseFloat(sF).toPrecision(2)), 10, 70);
             if (last_was_correct) {
-                context.fillText("Previous click: Success", 10, 90);
+                context.fillText("Previous click: Success", 10, 50);
             } else {
-                context.fillText("Previous click: Fail", 10, 90);
+                context.fillText("Previous click: Fail", 10, 50);
             }
             //context.fillText("Time to click: " + time_elapsed, 10, 110);
             
@@ -245,7 +289,14 @@ x = function() {
         } else {
             context.font = "20px serif"
             context.fillStyle = "black";
-            context.fillText("Click anywhere to continue.", 100, 70);
+            if (game_is_over) {
+                context.fillText("Average time taken per click: " + time_elapsed / (REPETITION_FACTOR * circles - 1) + "ms.", 10, 30);
+                context.fillText("Accuracy: " + (Math.floor(total_correct * 10000/(REPETITION_FACTOR * circles)) / 100) + "%", 10, 50);
+                context.fillText("Click anywhere to continue.", 10, 70);
+            } else {
+                context.fillText("Click anywhere to continue.", 10, 30);
+            }
+            
         }
 
         
